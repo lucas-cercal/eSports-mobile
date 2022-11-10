ddcafff8cfc9e4879080572c9a462a8b8e88a24c
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-09 22:25:57 -0300

    Criando componente e atualizando package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+  fontSize?: number;
+}
+
+function Button({ title, fontSize = 12 }: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={{ fontSize: fontSize }}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        Hello React Native!
+      </Text>
+      <Button title='Send 1' fontSize={12} />
+      <Button title='Send 2' fontSize={16} />
+      <Button title='Send 3' fontSize={20} />
+      <Button title='Hello World' fontSize={24} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +37,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
