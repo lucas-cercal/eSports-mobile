27dfe6c516c4753dfd96a7047e69c834291b7b85
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 11:49:48 -0300

    Adicionando componente

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { View, Image } from 'react-native';
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
 
 export function Home() {
   return (
@@ -10,6 +11,10 @@ export function Home() {
       <Image
         source={logoImg}
         style={styles.logo}
+      />
+      <Heading
+        title="Encontre o seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
     </View>
   );
