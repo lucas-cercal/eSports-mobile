4ab3dee86241b288fa89cee6c32549987c4ebddb
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-11 12:08:59 -0300

    Adicionando componente GameCard na Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,9 @@ import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 import { Heading } from '../../components/Heading';
+import { GameCard } from '../../components/GameCard';
+
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
@@ -15,6 +18,9 @@ export function Home() {
       <Heading
         title="Encontre o seu duo!"
         subtitle="Selecione o game que deseja jogar..."
+      />
+      <GameCard
+        data={GAMES[0]}
       />
     </View>
   );
