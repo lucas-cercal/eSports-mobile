d0c4afebb99924996798c6d7517c76e4627f28b8
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-11-12 14:40:48 -0300

    Adicionando FlatList com o DuoCard

--- src/components/DuoCard/index.tsx
+++ src/components/DuoCard/index.tsx
@@ -1,11 +1,42 @@
 import { View } from 'react-native';
+import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
 
-export function DuoCard() {
+export interface DuoCardProps {
+  hourEnd: string,
+  hourStart: string,
+  id: string,
+  name: string,
+  useVoiceChannel: boolean,
+  weekDays: string[],
+  yearsPlaying: number
+}
+
+interface Props {
+  data: DuoCardProps
+}
+
+export function DuoCard({ data }: Props) {
   return (
     <View style={styles.container}>
-      
+      <DuoInfo
+        label='Nome'
+        value={data.name}
+      />
+      <DuoInfo
+        label='Tempo de jogo'
+        value={`${data.yearsPlaying} anos`}
+      />
+      <DuoInfo
+        label='Disponibilidade'
+        value={`${data.weekDays.length} dias`}
+      />
+      <DuoInfo
+        label='Chamada de áudio?'
+        value='Sim'
+        colorValue="green"
+      />
     </View>
   );
 }
--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,7 +1,9 @@
-import { TouchableOpacity, View, Image } from 'react-native'
+import { useEffect, useState } from 'react'
+import { TouchableOpacity, View, Image, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRoute, useNavigation } from '@react-navigation/native'
 import { Entypo } from '@expo/vector-icons'
+
 
 import logoImg from '../../assets/logo-nlw-esports.png'
 
@@ -12,9 +14,11 @@ import { GameParams } from '../../@types/navigation'
 
 import { Background } from '../../components/Background'
 import { Heading } from '../../components/Heading'
-import { DuoCard } from '../../components/DuoCard'
+import { DuoCard, DuoCardProps } from '../../components/DuoCard'
 
 export function Game() {
+
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -23,6 +27,15 @@ export function Game() {
   function handleGoBack() {
     navigation.goBack();
   }
+
+  useEffect(() => {
+    fetch(`https://nlw-esports-server-backend.herokuapp.com/games/${game.id}/ads`)
+      .then(response => response.json())
+      .then(data => setDuos(data))
+      .catch((error) => {
+        console.log(error)
+      })
+  }, []);
 
   return (
     <Background>
@@ -57,7 +70,14 @@ export function Game() {
           subtitle='Conecte-se e comece a jogar!'
         />
 
-        <DuoCard />
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} />
+          )}
+        />
+
 
       </SafeAreaView>
     </Background>
